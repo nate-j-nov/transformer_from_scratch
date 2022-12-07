4a83e4d8c36bc5b5f6f8ee436c37be0aab4002cf
Nate Novak <nate.j.nov@gmail.com>
2022-12-06 21:31:27 -0500

    #1: Added FeedForwardLayer

---
+++ src/components/FeedForward.py
@@ -0,0 +1,24 @@
+# Brenden Collins // Nate Novak
+# CS7180 Advanced Perception
+# Fall 2022
+
+import torch.nn as nn
+
+class FeedForwardNetwork(nn.Module): 
+    ''' 
+    Class definition for a feed forward network: 
+    Documentation help: https://pytorch.org/docs/stable/generated/torch.nn.Linear.html
+    '''
+
+    def __init__(self):
+        super(FeedForwardNetwork, self).__init__(); 
+        self.linear1 = nn.Linear(512, 2048)
+        self.linear2 = nn.Linear(2048, 512)
+    
+    def forward(self, x): 
+        l1 = self.linear1(x)
+        relu = nn.ReLU(l1); 
+        return self.linear2(relu); 
+
+
+
