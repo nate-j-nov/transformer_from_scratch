879ea8eb3f71efc2403975258a8649f21639b39b
collinsbr <collins.brende@northeastern.edu>
2022-12-09 10:10:27 -0500

    added test file but it's empty

---
+++ src/components/test.py
@@ -0,0 +1,12 @@
+# Brenden Collins // Nate Novak
+# CS7180 Advanced Perception
+# Fall 2022
+#
+# Test various modules 
+
+from EncoderModule import EncoderModule
+
+def main():
+
+if __name__ == "__main__": 
+    main()
